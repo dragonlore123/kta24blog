dea2c65742693ff5541d27ff6f3b3ab327a7185e
dragonlore123 <dragonlore123@users.noreply.github.com>
2026-03-22 22:40:21 +0200

    Add auth

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,32 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PublicController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PublicController::class, 'index'])->name('home');
 Route::get('/about', [PublicController::class, 'about'])->name('about');
 
-Route::get('/admin/posts', [PostController::class, 'index'])->name('posts.index');
-Route::get('/admin/posts/create', [PostController::class, 'create'])->name('posts.create');
-Route::post('/admin/posts', [PostController::class, 'store'])->name('posts.store');
-Route::get('/admin/posts/{post}', [PostController::class, 'show'])->name('posts.show');
-Route::get('/admin/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
-Route::put('/admin/posts/{post}', [PostController::class, 'update'])->name('posts.update');
-Route::delete('/admin/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+// Route::get('/admin/posts', [PostController::class, 'index'])->name('posts.index');
+// Route::get('/admin/posts/create', [PostController::class, 'create'])->name('posts.create');
+// Route::post('/admin/posts', [PostController::class, 'store'])->name('posts.store');
+// Route::get('/admin/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+// Route::get('/admin/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+// Route::put('/admin/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+// Route::delete('/admin/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+Route::middleware('auth')->group(function () {
+    Route::resource('/admin/posts', PostController::class);
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
